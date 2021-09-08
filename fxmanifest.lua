05209fc2bc3f2d21da8cbd99d385f1e00ebf40e5
beoipisilon <48164586+beoipisilon@users.noreply.github.com>
2021-09-08 10:17:21 -0300

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version "adamant"
+game "gta5"
+
+ui_page "views/index.html"
+
+client_script "src/client.js"
+files {
+	"views/index.html",
+	"views/js/script.js",
+    "views/css/styles.css",
+}
